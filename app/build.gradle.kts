8949342ce8d3b537561c42a9e3f900df02afd7be
Pronay sarker <pronaycoding@gmail.com>
2025-02-24 03:10:30 +0600

    change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.pronaycoding.blanket_mobile"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
